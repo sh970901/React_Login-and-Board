501c6b01b3dc5fac4c2e018ab333a4832eae9b1b
sh970901 <aaddss639@naver.com>
2022-01-24 20:52:08 +0900

    ADD comments

--- client/src/components/BoardDetail.js
+++ client/src/components/BoardDetail.js
@@ -1,17 +1,56 @@
 import React, { useEffect, useState } from 'react';
-import { Button } from 'react-bootstrap';
+import { Button} from 'react-bootstrap';
 import { Link, useLocation } from 'react-router-dom';
 import { useHistory } from 'react-router-dom';
-
+import axios from 'axios';
+import Comment from './Comment';
+import { Table } from 'react-bootstrap';
 
 const BoardDetail = () => {
     const history = useHistory()
     const location = useLocation();
+    const [comment, setComment] = useState("")
+    const [comments, setComments] = useState("")
+
+    useEffect(()=>{
+        fetch(`http://localhost:5000/api/comment/${location.state.props.title}`)
+        .then(res=>res.json())
+        .then(data=> {
+            setComments(data)
+        })
+    },[])
+    
+    function handleComment(e){
+        e.preventDefault();
+        setComment(e.target.value)
+    }
+    function sendComment(e){
+        e.preventDefault();
+        console.log(comment)
+        if(comment === ""){
+            alert("값을 입력하세요")
+        }else{
+            const userData = {
+                id: sessionStorage.getItem("user_id"),
+                comment: comment,
+                title: location.state.props.title
+            }
+            axios.post("http://localhost:5000/api/user/comment",userData)
+            .then((res)=>{
+                if(res.status ===200){
+                    alert("댓글 생성완료")
+                    window.location.reload()
+                    
+                }
+            })
+            
+        
+        }
+    }
     function deleteBoard(e) {
         e.preventDefault();
         
         if (sessionStorage.getItem('user_id') === location.state.props.writer) {
-            
             const url = `http://localhost:5000/api/board/user/${location.state.props.title}`
             fetch(url, {
                 method: 'DELETE'
@@ -21,7 +60,6 @@ const BoardDetail = () => {
         } else {
             alert('자신의 글만 삭제가능합니다.')
         }
-
     }
     return (
         <div>
@@ -36,6 +74,34 @@ const BoardDetail = () => {
             <h2>내용</h2>
             <input type='text' value={location.state.props.content} readOnly></input>
             <br /><br />
+            <Table striped bordered hover size="sm">
+                <thead>
+                    <tr>
+                        <th>작성자</th>
+                        <th>댓글</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {comments ? comments.map(c=>{
+                        return (<Comment
+                        
+                        id = {c.id}
+                        comment={c.comments}
+                        ></Comment>)
+                    }): 
+                    <tr>
+                        <td>
+                            댓글 없음
+                        </td>    
+                    </tr>}
+                </tbody>
+            </Table>
+            
+            <h2>댓글</h2>
+            <input type="text" value={comment} onChange={handleComment}></input>      
+            <br></br>
+            <Button onClick={sendComment}>댓글 달기</Button><br></br><br></br> 
+
             <Link to='/boardRead'>
                 <Button>뒤로가기</Button>{'  '}
             </Link>
